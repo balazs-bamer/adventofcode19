ebb5bae6f6ff6ee2d9f78c19525f475995337ea6
Balazs Bamer <balazs@bamer.hu>
2019-12-31 12:55:30 +0100

    20th day.

--- dec20-1.cpp
+++ dec20-1.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <limits>
 #include <vector>
+#include <iomanip>
 #include <utility>
 #include <fstream>
 #include <iostream>
@@ -12,11 +13,16 @@
 #include <algorithm>
 #include <unordered_map>
 
+// I assume there are no adjacent teleport labels.
+
 class Coordinates final {
+public:
+  static int constexpr cDirNorth = 0;
+  static int constexpr cDirEast  = 1;
+  static int constexpr cDirSouth = 2;
+  static int constexpr cDirWest  = 3;
+
 private:
-  static uint64_t constexpr cShiftX = 32u;
-  static uint64_t constexpr cShiftY = 48u;
-  static uint64_t constexpr cMask   = 0xffffu;
   static int constexpr cDeltaX[]    = { 0, 1, 0, -1};
   static int constexpr cDeltaY[]    = {-1, 0,  1, 0};
 
@@ -28,15 +34,6 @@ public:
   }
 
   Coordinates(int const aX, int const aY) noexcept : mX(aX), mY(aY) {
-  }
-
-  Coordinates(uint64_t const aCompact) noexcept : mX((aCompact >> cShiftX) & cMask), mY((aCompact >> cShiftY) & cMask) {
-  }
-
-  Coordinates& operator=(uint64_t const aCompact) noexcept {
-    mX = (aCompact >> cShiftX) & cMask;
-    mY = (aCompact >> cShiftY) & cMask;
-    return *this;
   }
 
   Coordinates operator+(int const aDirection) const noexcept {
@@ -68,10 +65,6 @@ public:
   bool operator!=(Coordinates const &aOther) const noexcept {
     return mX != aOther.mX || mY != aOther.mY;
   }
-
-  explicit operator uint64_t() const noexcept {
-    return (static_cast<uint64_t>(mX) & cMask) << cShiftX | (static_cast<uint64_t>(mY) & cMask) << cShiftY;
-  }
 };
 
 int constexpr Coordinates::cDeltaX[];
@@ -87,59 +80,42 @@ struct std::hash<Coordinates> {
 
 class Labyrinth final {
 private:
-  static char     constexpr cNewLine          = '\n';
-  static char     constexpr cCorridor         = '.';
-  static char     constexpr cWall             = '#';
-  static int      constexpr cDirCount         =  4;
+  static uint8_t  constexpr cNewLine          = '\n';
+  static uint8_t  constexpr cCorridor         = '.';
+  static uint8_t  constexpr cWall             = '#';
+  static uint8_t  constexpr cSpace            = ' ';
+  static size_t   constexpr cDirCount         =   4u;
   static size_t   constexpr cInvalidResult    = std::numeric_limits<size_t>::max();
-  static size_t   constexpr cMatrixSize       = cCompactOffset;
+  static uint16_t constexpr cWhichPortMask    = 128u;
+  static uint16_t constexpr cFirstPort        = 256u;
+  static uint16_t constexpr cPortIncrement    = 256u;
+  static char     constexpr cStartLabel[]     = "AA";
+  static char     constexpr cTargetLabel[]    = "ZZ";
 
-  std::deque<std::deque<uint16_t>> mMap;
+  std::deque<std::deque<uint8_t>>           mRawMap;
+  std::vector<std::vector<uint16_t>>        mWarpMap;
+  uint16_t                                  mNextPort = cFirstPort;
+  std::unordered_map<std::string, uint16_t> mLabel2port;
+  std::unordered_map<uint16_t, Coordinates> mPort2coord;
 
 public:
   Labyrinth(std::ifstream &aIn) {
     bool newLine = true;
-    bool foundStart = false;
     while(true) {
       if(newLine) {
-        mMap.emplace_back(std::deque<char>());
+        mRawMap.emplace_back(std::deque<uint8_t>());
+        mRawMap.back().push_back(cSpace);
         newLine = false;
       }
       else { // nothing to do
       }
-      char input = aIn.get();
+      uint8_t input = aIn.get();
       if(input == cNewLine) {
+        mRawMap.back().push_back(cSpace);
         newLine = true;
-        if(!foundStart) {
-          ++mStartY;
-          mStartX = 0;
-        }
-        else { // nothing to do
-        }
       }
       else {
-        if(input == cStart) {
-          foundStart = true;
-          input = cCompactStart;
-        }
-        else {
-          mStartX += (foundStart ? 0 : 1);
-          if(input == cWall) {
-            input = cCompactWall;
-          }
-          else if(input == cCorridor) {
-            input = cCompactCorridor;
-          }
-          else {
-            input -= cCompactOffset;
-            if(input >= cCompactKeyStart) {
-              mKeyMask |= static_cast<uint64_t>(1u) << (input - cCompactKeyStart);
-            }
-            else { // nothing to do
-            }
-          }
-        }
-        mMap.back().push_back(input);
+        mRawMap.back().push_back(input);
       }
       if(!aIn.good()) {
         break;
@@ -147,29 +123,118 @@ public:
       else { // nothing to do
       }
     }
-    while(mMap.back().size() <= 1u) {
-      mMap.pop_back();
+    while(mRawMap.back().size() <= 2u) {
+      mRawMap.pop_back();
+    }
+    std::deque<uint8_t> line(mRawMap.back().size(), cSpace);
+    mRawMap.push_front(line);
+    mRawMap.push_back(line);
+  }
+
+  void warp() {
+    {
+      std::vector<uint16_t> line(mRawMap.front().size(), cSpace);
+      mWarpMap = std::vector<std::vector<uint16_t>>(mRawMap.size(), line);
+    }
+    for(size_t y = 0u; y < mRawMap.size(); ++y) {
+      for(size_t x = 0u; x < mRawMap.front().size(); ++x) {
+        uint8_t foundItem = mRawMap[y][x];
+        if(std::isalpha(foundItem)) {
+          Coordinates foundLoc(x, y);
+          Coordinates otherLoc;
+          uint8_t otherItem;
+          for(size_t i = 0u; i < cDirCount; ++i) {
+            otherLoc = foundLoc + i;
+            otherItem = mRawMap[otherLoc.getY()][otherLoc.getX()];
+            if(std::isalpha(otherItem)) {
+              break;
+            }
+            else { // nothing to do
+            }
+          }
+          Coordinates labelLoc;
+          Coordinates corridorLoc;
+          std::string label;
+          if(foundLoc.getX() < otherLoc.getX() || foundLoc.getY() < otherLoc.getY()) {
+            label += foundItem;
+            label += otherItem;
+          }
+          else {
+            label += otherItem;
+            label += foundItem;
+          }
+          Coordinates otherHalfLoc;
+          if(foundLoc.getY() == otherLoc.getY()) {
+            if(mRawMap[foundLoc.getY()][foundLoc.getX() - 1] == cCorridor) {
+              corridorLoc = foundLoc + Coordinates::cDirWest;
+              labelLoc = foundLoc;
+              otherHalfLoc = otherLoc;
+            }
+            else {
+              corridorLoc = otherLoc + Coordinates::cDirEast;
+              labelLoc = otherLoc;
+              otherHalfLoc = foundLoc;
+            }
+          }
+          else {
+            if(mRawMap[foundLoc.getY() - 1][foundLoc.getX()] == cCorridor) {
+              corridorLoc = foundLoc + Coordinates::cDirNorth;
+              labelLoc = foundLoc;
+              otherHalfLoc = otherLoc;
+            }
+            else {
+              corridorLoc = otherLoc + Coordinates::cDirSouth;
+              labelLoc = otherLoc;
+              otherHalfLoc = foundLoc;
+            }
+          }
+          if(mWarpMap[otherHalfLoc.getY()][otherHalfLoc.getX()] == cSpace && mWarpMap[labelLoc.getY()][labelLoc.getX()] == cSpace) {
+            auto found = mLabel2port.find(label);
+            if(found == mLabel2port.end()) {
+              mLabel2port[label] = mNextPort;
+              mPort2coord[mNextPort | cWhichPortMask] = corridorLoc;
+              mWarpMap[labelLoc.getY()][labelLoc.getX()] = mNextPort;
+              mWarpMap[otherHalfLoc.getY()][otherHalfLoc.getX()] = mNextPort;
+              mNextPort += cPortIncrement;
+            }
+            else {
+              uint16_t pairPort = found->second;
+              auto found = mPort2coord.find(pairPort);
+              if(found == mPort2coord.end()) {
+                mPort2coord[pairPort] = corridorLoc;
+                mWarpMap[labelLoc.getY()][labelLoc.getX()] = (pairPort | cWhichPortMask);
+                mWarpMap[otherHalfLoc.getY()][otherHalfLoc.getX()] = (pairPort | cWhichPortMask);
+              }
+              else { // nothing to do
+              }
+            }
+          }
+          else { // nothing to do
+          }
+        }
+        else {
+          mWarpMap[y][x] = foundItem;
+        }
+      }
     }
   }
 
   size_t calculateShortestPath() {
+    Coordinates start = mPort2coord[mLabel2port[cStartLabel] | cWhichPortMask];
+    Coordinates target = mPort2coord[mLabel2port[cTargetLabel] | cWhichPortMask];
     size_t iterations = 0u;
     size_t result = cInvalidResult;
-    uint64_t lastNode;
-    Coordinates start(mStartX, mStartY);
-    std::unordered_map<uint64_t, size_t> costs; // assume cInvalidResult for nodes not present here
-    std::multimap<size_t, uint64_t> queue;
-    std::unordered_map<uint64_t, uint64_t> previous;
-    uint64_t startValue = static_cast<uint64_t>(start);
-    costs[startValue] = 0u;
-    queue.insert(std::pair<size_t, uint64_t>(0u, startValue));
-    previous[startValue] = std::numeric_limits<uint64_t>::max();
+    Coordinates lastNode;
+    std::unordered_map<Coordinates, size_t> costs; // assume cInvalidResult for nodes not present here
+    std::multimap<size_t, Coordinates> queue;
+    costs[start] = 0u;
+    queue.insert(std::pair<size_t, Coordinates>(0u, start));
     while(!queue.empty()) {
       auto smallest = queue.begin();
       size_t smallestCost = smallest->first;
-      uint64_t smallestNode = smallest->second;
+      Coordinates smallestNode = smallest->second;
       queue.erase(smallest);
-      if((smallestNode & mKeyMask) == mKeyMask) {
+      if(smallestNode == target) {
         if(smallestCost < result) {
           result = smallestCost;
           lastNode = smallestNode;
@@ -181,25 +246,32 @@ public:
       }
       Coordinates smallestCoord(smallestNode);
       for(size_t i = 0; i < cDirCount; ++i) {
-        Coordinates otherCoord = smallestCoord + i;
-        char found = mMap[otherCoord.getY()][otherCoord.getX()];
-        if(found != cCompactWall && (found >= cCompactKeyStart || found == cCompactStart || (smallestNode & (1u << (found - cCompactDoorStart))) != 0u)) {
-          uint64_t otherNode = (static_cast<uint64_t>(otherCoord) | (smallestNode & mKeyMask) | (found >= cCompactKeyStart ? 1u << (found - cCompactKeyStart) : 0u));
-          auto other = costs.find(otherNode);
-          size_t otherCost = (other == costs.end() ? cInvalidResult : other->second);
-          size_t newCost = smallestCost + 1u;
-          if(newCost < otherCost) {
-            if(other == costs.end()) {
-              costs[otherNode] = newCost;
-            }
-            else {
-              other->second = newCost;
-            }
-            queue.insert(std::pair<size_t, uint64_t>(newCost, otherNode));
-            previous[otherNode] = smallestNode;
+        Coordinates otherNode = smallestCoord + i;
+        uint16_t found = mWarpMap[otherNode.getY()][otherNode.getX()];
+        if(found == cSpace || found == cWall) {
+          continue;
+        }
+        else if(found != cCorridor) {
+          otherNode = mPort2coord[found];
+          if(otherNode.isOrigin()) {
+            continue;
           }
           else { // nothing to do
           }
+        }
+        else { // nothing to do
+        }
+        auto other = costs.find(otherNode);
+        size_t otherCost = (other == costs.end() ? cInvalidResult : other->second);
+        size_t newCost = smallestCost + 1u;
+        if(newCost < otherCost) {
+          if(other == costs.end()) {
+            costs[otherNode] = newCost;
+          }
+          else {
+            other->second = newCost;
+          }
+          queue.insert(std::pair<size_t, Coordinates>(newCost, otherNode));
         }
         else { // nothing to do
         }
@@ -207,23 +279,15 @@ public:
       ++iterations;
     }
 std::cout << "iter: " << iterations << '\n';
-    for(uint64_t node = lastNode; node != std::numeric_limits<uint64_t>::max(); node = previous[node]) {
-      Coordinates coord = node;
-      char found = mMap[coord.getY()][coord.getX()];
-      if(found != cCompactCorridor) {
-        std::cout << (char)(cCompactOffset + found) << ' ';
-      }
-      else { // nothing to do
-      }
-    }
 std::cout << '\n';
     return result;
   }
-
-private:
 };
   
-size_t constexpr Labyrinth::cInvalidResult;
+size_t  constexpr Labyrinth::cInvalidResult;
+uint8_t constexpr Labyrinth::cSpace;
+char    constexpr Labyrinth::cStartLabel[];
+char    constexpr Labyrinth::cTargetLabel[];
 
 int main(int const argc, char **argv) {
   try {
@@ -235,6 +299,7 @@ int main(int const argc, char **argv) {
     std::ifstream in(argv[1]);
     Labyrinth labyrinth(in);
     auto begin = std::chrono::high_resolution_clock::now();
+    labyrinth.warp();
     size_t length = labyrinth.calculateShortestPath();
     auto end = std::chrono::high_resolution_clock::now();
     auto timeSpan = std::chrono::duration_cast<std::chrono::duration<double>>(end - begin);

1e8f8dd1fa9c35505a98ef8ec1a22d757fd7d610
Balazs Bamer <balazs@bamer.hu>
2020-01-11 17:11:19 +0100

    Day 24 part 2 is ready. Exactly reproduces the ten-minute example run in the problem text with its bug count, but somehow fails on the real problem. Can't understand how it can be.

--- dec24-2.cpp
+++ dec24-2.cpp
@@ -10,7 +10,7 @@
 
 class Population final {
 private: 
-  static constexpr size_t   cMinutes          = 5u; // 200u;
+  static constexpr size_t   cMinutes          = 200u;
   static constexpr uint64_t cInitNeighbour    =   0ull;
   static constexpr uint64_t cSize             =   5ull;
   static constexpr uint64_t cBitsPerPlace     =   2ull;
@@ -20,14 +20,13 @@ private:
   static constexpr uint64_t cCrunchVertShift  =  29ull;
   static constexpr uint64_t cSouth2northShift =  40ull;
   static constexpr uint64_t cEast2westShift   =   8ull;
-  static constexpr uint64_t cCenterNorthShift =  15ull;
-  static constexpr uint64_t cCenterWestShift  =  23ull;
-  static constexpr uint64_t cCenterEastShift  =  27ull;
-  static constexpr uint64_t cCenterSouthShift =  35ull;
+  static constexpr uint64_t cCenterNorthShift =  14ull;
+  static constexpr uint64_t cCenterWestShift  =  22ull;
+  static constexpr uint64_t cCenterEastShift  =  26ull;
+  static constexpr uint64_t cCenterSouthShift =  34ull;
   static constexpr uint64_t cLeftMask         = 0x0000ff3f6ff3f6ffull;
   static constexpr uint64_t cRightMask        = 0x0003f6ff3f6ff3f6ull;
-  static constexpr uint64_t cBugMask          = 0x0001555555555555ull;
-  static constexpr uint64_t cPopulationMask   = 0x0003ffffffffffffull;
+  static constexpr uint64_t cBugMask          = 0x0001555554555555ull;
   static constexpr uint64_t cCenterNorthMask  = 0x0000000000004000ull;
   static constexpr uint64_t cCenterWestMask   = 0x0000000000400000ull;
   static constexpr uint64_t cCenterMask       = 0x0000000001000000ull;
@@ -42,12 +41,13 @@ private:
   static constexpr size_t   cHorLookupWidth   = 5u;
   static constexpr size_t   cVertLookupSize   = 1ull << 21u;
                                             //   fedcba9876543210
-  static constexpr uint64_t cHorLookup[1 << cHorLookupWidth]   = { 0u, 1u, 1u, 2u, 1u, 2u, 2u, 3u,
-                                                                   1u, 2u, 2u, 3u, 2u, 3u, 3u, 3u,
-                                                                   1u, 2u, 2u, 3u, 2u, 3u, 3u, 3u,
-                                                                   2u, 3u, 3u, 3u, 3u, 3u, 3u, 3u };
+  static constexpr uint64_t cHorLookup[1u << cHorLookupWidth]   = { 0u, 1u, 1u, 2u, 1u, 2u, 2u, 3u,
+                                                                    1u, 2u, 2u, 3u, 2u, 3u, 3u, 3u,
+                                                                    1u, 2u, 2u, 3u, 2u, 3u, 3u, 3u,
+                                                                    2u, 3u, 3u, 3u, 3u, 3u, 3u, 3u };
   std::array<uint8_t, cVertLookupSize> mVertLookup;
   uint64_t                             mInitialPopulation;
+  uint64_t                             mBitsPerByte[256];
 
 public:
   Population(std::ifstream &aIn) {
@@ -67,19 +67,23 @@ public:
       }
     }
     if(readSoFar != cSize) {
-      throw std::invalid_argument("Invalid initial popuilation");
+      throw std::invalid_argument("Invalid initial population");
     }
     else { // nothing to do
     }
-    mInitialPopulation &= cPopulationMask;
+    mInitialPopulation &= cBugMask;
     std::fill(mVertLookup.begin(), mVertLookup.end(), 0u);
     uint64_t const bitConverter[] = { 0u, 1u, 10u, 11u, 20u };
-    for(uint64_t i = 0u; i < cCrunchHorMask; ++i) {
+    for(uint64_t i = 0u; i <= cCrunchHorMask; ++i) {
       size_t vertIndex = 0u;
       for(size_t j = 0; j < cHorLookupWidth; ++j) {
         vertIndex += ((i & (1u << j)) > 0u ? 1u << bitConverter[j] : 0u);
       }
       mVertLookup[vertIndex] = cHorLookup[i];
+    }
+    mBitsPerByte[0] = 0u;
+    for(size_t i = 0; i < 256; ++i) {
+      mBitsPerByte[i] = (i & 1u) + mBitsPerByte[i / 2u];
     }
   }
 
@@ -87,21 +91,18 @@ public:
     std::array<std::list<uint64_t>, 2u> levels;
     size_t current = 0u;
     levels[current].push_back(mInitialPopulation);
+// print("init ", mInitialPopulation);
     for(size_t i = 0; i < cMinutes; ++i) {
-std::cout << "------------------ MINUTE: " << i << '\n';
+//std::cout << "------------------ MINUTE: " << i << '\n';
       levels[1u - current].clear();
       levels[current].push_front(cInitNeighbour);
       levels[current].push_back(cInitNeighbour);
       for(auto level = levels[current].begin(); level != levels[current].end(); ++level) {
         uint64_t sum = getOwnSum(*level);
-std::cout << "this: " << std::hex << std::setw(16) << *level;
-std::cout << std::hex << std::setw(16) << sum << '\n';
         uint64_t correctionSum = 0u;
         if(level != levels[current].begin()) {
           auto previous = level;
           --previous;
-std::cout << "prev: " << std::hex << std::setw(16) << *previous;
-std::cout << std::hex << std::setw(16) << getOuterSum(*previous) << '\n';
           correctionSum |= getOuterSum(*previous);
         }
         else { // nothing to do
@@ -110,13 +111,11 @@ std::cout << std::hex << std::setw(16) << getOuterSum(*previous) << '\n';
         ++next;
         if(next != levels[current].end()) {
           correctionSum |= getInnerSum(*next);
-std::cout << "next: " << std::hex << std::setw(16) << *next;
-std::cout << std::hex << std::setw(16) << getInnerSum(*next) << '\n';
         }
         else { // nothing to do
         }
         levels[1u - current].push_back(merge(*level, sum, correctionSum));
-std::cout << "res : " << std::hex << std::setw(16) << *level << '\n';
+//print(" res ", merge(*level, sum, correctionSum));
       }
       current = 1u - current;
     }
@@ -129,67 +128,108 @@ std::cout << "res : " << std::hex << std::setw(16) << *level << '\n';
 
 private:
   uint64_t getOwnSum(uint64_t const aPopulation) const noexcept {
-    uint64_t population = aPopulation & ~cCenterMask;
-    uint64_t sum = population << cVerticalShift;
-    sum += (population >> cVerticalShift);
-    sum += (population & cLeftMask) << 2u;
-    uint64_t right = (population & cRightMask) >> 2u;
-    uint64_t upperBits = (sum >> cBugShift) & cBugMask;
-    uint64_t lowerBits = sum & cBugMask;
-    uint64_t already3 = upperBits & lowerBits;
-    right &= ~already3;
-    return sum + right;
+    uint64_t result;
+    if(aPopulation > 0u) {
+      uint64_t population = aPopulation & ~cCenterMask;
+      uint64_t sum = population << cVerticalShift;
+      sum += (population >> cVerticalShift);
+      sum += (population & cLeftMask) << 2u;
+      uint64_t right = (population & cRightMask) >> 2u;
+      uint64_t upperBits = (sum >> cBugShift) & cBugMask;
+      uint64_t lowerBits = sum & cBugMask;
+      uint64_t already3 = upperBits & lowerBits;
+      right &= ~already3;
+      result = sum + right;
+    }
+    else {
+      result = 0u;
+    }
+    return result;
   }
 
   // Four big tiles (from arg) influence each normal on the perimeter
   uint64_t getOuterSum(uint64_t const aPopulation) const noexcept {
-    uint64_t sum  = ((aPopulation >> cCenterNorthShift) & 1u) * cSideNorthMask;
-             sum += ((aPopulation >> cCenterWestShift) & 1u)  * cSideWestMask;
-             sum += ((aPopulation >> cCenterEastShift) & 1u)  * cSideEastMask;
-             sum += ((aPopulation >> cCenterSouthShift) & 1u) * cSideSouthMask;
-    return sum;
+    uint64_t result;
+    if(aPopulation > 0u) {
+      uint64_t sum  = ((aPopulation >> cCenterNorthShift) & 1u) * cSideNorthMask;
+               sum += ((aPopulation >> cCenterWestShift) & 1u)  * cSideWestMask;
+               sum += ((aPopulation >> cCenterEastShift) & 1u)  * cSideEastMask;
+               sum += ((aPopulation >> cCenterSouthShift) & 1u) * cSideSouthMask;
+      result = sum;
+    }
+    else {
+      result = 0u;
+    }
+    return result;
   }
 
   // The small tiles on the inner level perimeter (from arg) influence the four normal ones around the center
   uint64_t getInnerSum(uint64_t const aPopulation) const noexcept {
-    uint64_t north =  aPopulation & cSideNorthMask;
-    uint64_t south = (aPopulation & cSideSouthMask) >> cSouth2northShift;
-    uint64_t west  =  aPopulation & cSideWestMask;
-    uint64_t east  = (aPopulation & cSideEastMask) >> cEast2westShift;
-    uint64_t sum  = cHorLookup[(north | north >> cCrunchHorShift) & cCrunchHorMask] * cCenterNorthMask;
-             sum |= cHorLookup[(south | south >> cCrunchHorShift) & cCrunchHorMask] * cCenterSouthMask;
-             sum |= mVertLookup[(west | west >> cCrunchVertShift) & cCrunchVertMask] * cCenterWestMask;
-             sum |= mVertLookup[(east | east >> cCrunchVertShift) & cCrunchVertMask] * cCenterWestMask;
-    return sum;
-  }
-
-  uint64_t merge(uint64_t const aPopulation, uint64_t const aSumA, uint64_t const aSumB) const noexcept {
-    uint64_t lowerBitsA = aSumA & cBugMask;
-    uint64_t upperBitsA = (aSumA >> cBugShift) & cBugMask;
-    uint64_t lowerBitsB = aSumB & cBugMask;
-    uint64_t upperBitsB = (aSumB >> cBugShift) & cBugMask;
-    uint64_t nonzeroA = lowerBitsA | upperBitsA;
-    uint64_t nonzeroB = lowerBitsB | upperBitsB;
-    uint64_t lowerBits = (upperBitsA & nonzeroB) | (upperBitsB & nonzeroA) | (lowerBitsA & !nonzeroB) | (lowerBitsB & !nonzeroA);
-    uint64_t upperBits = upperBitsA | upperBitsB | (lowerBitsA & lowerBitsB);
-    uint64_t newPopulation = aPopulation & lowerBits & ~upperBits;
-    newPopulation |= (~aPopulation & (lowerBits ^ upperBits)) & cPopulationMask;
-    return newPopulation & ~cCenterMask;
-  }
-
-  uint64_t countBits(uint64_t const aValue) const noexcept {
-    uint64_t bitsPerByte[256];
-    bitsPerByte[0] = 0u;
-    for(size_t i = 0; i < 256; ++i) {
-      bitsPerByte[i] = (i & 1u) + bitsPerByte[i / 2u];
+    uint64_t result;
+    if(aPopulation > 0u) {
+      uint64_t north =  aPopulation & cSideNorthMask;
+      uint64_t south = (aPopulation & cSideSouthMask) >> cSouth2northShift;
+      uint64_t west  =  aPopulation & cSideWestMask;
+      uint64_t east  = (aPopulation & cSideEastMask) >> cEast2westShift;
+      uint64_t sum  = cHorLookup[(north | north >> cCrunchHorShift) & cCrunchHorMask] * cCenterNorthMask;
+               sum |= cHorLookup[(south | south >> cCrunchHorShift) & cCrunchHorMask] * cCenterSouthMask;
+               sum |= mVertLookup[(west | west >> cCrunchVertShift) & cCrunchVertMask] * cCenterWestMask;
+               sum |= mVertLookup[(east | east >> cCrunchVertShift) & cCrunchVertMask] * cCenterEastMask;
+      result = sum;
     }
-    uint64_t result = 0u;
-    for(size_t i = 0; i < sizeof(uint64_t); ++i) {
-      result += bitsPerByte[(aValue >> (i * 8u)) & 255];
+    else {
+      result = 0u;
     }
     return result;
   }
+
+  uint64_t merge(uint64_t const aPopulation, uint64_t const aSumA, uint64_t const aSumB) const noexcept {
+    uint64_t lowerBitsA = aSumA;
+    uint64_t upperBitsA = aSumA >> cBugShift;
+    uint64_t lowerBitsB = aSumB;
+    uint64_t upperBitsB = aSumB >> cBugShift;
+    uint64_t nonzeroA = lowerBitsA | upperBitsA;
+    uint64_t nonzeroB = lowerBitsB | upperBitsB;
+    uint64_t lowerBits = 
+      (upperBitsA & nonzeroB)
+    | (upperBitsB & nonzeroA)
+    | (lowerBitsA & ~nonzeroB)
+    | (lowerBitsB & ~nonzeroA);
+    uint64_t upperBits = upperBitsA | upperBitsB | (lowerBitsA & lowerBitsB);
+    uint64_t newPopulation = aPopulation & lowerBits & ~upperBits;
+    newPopulation |= ~aPopulation & (lowerBits ^ upperBits);
+    return newPopulation & cBugMask;
+  }
+
+  uint64_t countBits(uint64_t const aValue) const noexcept {
+    uint64_t result = 0u;
+    for(size_t i = 0; i < sizeof(uint64_t); ++i) {
+      result += mBitsPerByte[(aValue >> (i * 8u)) & 255];
+    }
+    return result;
+  }
+  
+  void print(char const * const aPrefix, uint64_t const aValue) const {
+if(aValue == 0u) return;
+    for(uint64_t i = 0; i < cSize * cSize; ++i) {
+      if(i % cSize == 0u) {
+        std::cout << aPrefix;
+      }
+      else { // ntd
+      }
+      std::cout //<< ((aValue >> (2u * i + 1u) & 1u) > 0u ? '#' : ':')
+                << ((aValue >> (2u * i) & 1u) > 0u ? '#' : ':');// << ' ';
+      if(i % cSize == cSize - 1u) {
+        std::cout << '\n';
+      }
+      else { // ntd
+      }
+    }
+std::cout << "- - - -\n";
+  }
 };
+
+constexpr uint64_t Population:: cInitNeighbour;
   
 constexpr uint64_t Population::cHorLookup[];
 
